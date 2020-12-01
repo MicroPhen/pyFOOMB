5f6d2aa2a2b38c5be14a0d1269ccc5148186a476
Johannes Hemmerich <hemmerich@outlook.com>
2020-12-01 11:40:46 +0100

    adds models for testing model_checking module

--- tests/modelling_library.py
+++ tests/modelling_library.py
@@ -89,6 +89,17 @@ class Model06(BioprocessModel):
         return [y0, y1]
 
 
+class Model06_V02(Model06):
+    # The auto-detection of event will fail, so the user need to provided the initial switches
+    def state_events(self, t, y, sw):
+        y0, y1 = y
+        event_t1 = t - 5
+        event_t2 = t - 2
+        event_t3 = t - 1
+        events = [event_t1, event_t2, event_t3]
+        return events
+
+
 class Model07(Model02):
 
     def state_events(self, t, y, sw):
@@ -339,6 +350,103 @@ class Model03_BadV09(Model03_V06):
         return np.array([event_y0, event_y1, event_t,])
 
 
+class Model06_Bad01(Model06):
+    # Has an undefined variable
+    def rhs(self, t, y, sw):
+        y0, y1 = y
+        rate0, rate1 = self.model_parameters.to_numpy()
+        if sw[0]:
+            dy0dt = rate99
+        else:
+            dy0dt = rate0
+
+        if sw[1]:
+            dy1dt = rate0
+        else:
+            dy1dt = rate1
+        return np.array([dy0dt, dy1dt])
+
+
+class Model06_Bad02(Model06):
+    # Has an undefined variable
+    def rhs(self, t, y, sw):
+        y0, y1 = y
+        rate0, rate1 = self.model_parameters.to_numpy()
+        if sw[0]:
+            dy0dt = rate1
+        else:
+            dy0dt = rate99
+
+        if sw[1]:
+            dy1dt = rate0
+        else:
+            dy1dt = rate1
+        return np.array([dy0dt, dy1dt])
+
+
+class Model06_Bad03(Model06):
+
+    # Has an undefined variable
+    def state_events(self, t, y, sw):
+        y0, y1 = y
+        event_t1 = t - 5
+        event_t3 = t - 1
+        return np.array([event_t1, event_t2, event_t3])  
+
+
+class Model06_Bad04(Model06):
+    # Has an undefined variable
+    def change_states(self, t, y, sw):
+        y0, y1 = y
+        y00 = self.initial_values['y00']
+        y10 = self.initial_values['y10']
+
+        if sw[1]:
+            y0 = y000000000000000
+            y1 = y10
+
+        return [y0, y1]
+
+
+class Model06_Bad05(Model06):
+    # the number of events depends on the switches
+    def state_events(self, t, y, sw):
+        y0, y1 = y
+        event_t1 = t - 5
+        event_t2 = t - 2
+        event_t3 = t - 1
+        if sw[1]:
+            events = [event_t1, event_t3]
+        else:
+            events = [event_t1, event_t2, event_t3]
+        return events 
+
+
+class Model06_Bad06(Model06):
+    # Inconsitent parameter unpacking
+    def change_states(self, t, y, sw):
+        y0, y1 = y
+        rate00000 = self.model_parameters['rate0']
+        return [y0, y1]
+
+
+class Model06_Bad07(Model06):
+    # Inconsitent parameter unpacking
+    def change_states(self, t, y, sw):
+        y0, y1 = y
+        rate1, rate0 = self.model_parameters.to_numpy()
+        return [y0, y1]
+
+
+class Model06_Bad08(Model06):
+    # Inconsitent state unpacking
+    def change_states(self, t, y, sw):
+        y1, y0 = y
+        rate0 = self.model_parameters['rate0']
+        return [y0, y1]
+
+
+
 class ModelLibrary():
 
     modelnames = [
@@ -414,6 +522,17 @@ class ModelLibrary():
         Model03_BadV09,
     ]
 
+    bad_variants_model06 = [
+        Model06_Bad01,
+        Model06_Bad02,
+        Model06_Bad03,
+        Model06_Bad04,
+        Model06_Bad05,
+        Model06_Bad06,
+        Model06_Bad07,
+        Model06_Bad08,
+    ]
+
     variants_model03 = [
         Model03_V02,
         Model03_V03,
@@ -438,21 +557,90 @@ class ObservationFunction01(ObservationFunction):
         return state_values * slope_01 + offset_01
 
 
+class ObservationFunction02(ObservationFunction):
+    def observe(self, state_values):
+        p1, p2, p3, p4, p5 = self.observation_parameters.to_numpy()
+        return state_values + p1 + p2 + p3 + p4 + p5
+
+
+class ObservationFunction02_V02(ObservationFunction):
+    def observe(self, state_values):
+        p1, \
+            p2, p3, \
+            p4, p5 \
+            = self.observation_parameters.to_numpy()
+        return state_values + p1 + p2 + p3 + p4 + p5
+
+
+class ObservationFunction02_V03(ObservationFunction):
+    def observe(self, state_values):
+        p1, \
+            p2, p3, \
+            p4, p5 = self.observation_parameters.to_numpy()
+        return state_values + p1 + p2 + p3 + p4 + p5
+
+
+class ObservationFunction02_V04(ObservationFunction):
+    def observe(self, state_values):
+        p1 = self.observation_parameters['p1']
+        p2 = self.observation_parameters['p2']
+        p3 = self.observation_parameters['p3']
+        p4 = self.observation_parameters['p4']
+        p5 = self.observation_parameters['p5']
+        return state_values + p1 + p2 + p3 + p4 + p5
+
+
+class ObservationFunction01_Bad01(ObservationFunction):
+    # observation parameters are unpacked in the wrong order
+    def observe(self, model_values):
+        slope_01, offset_01 = self.observation_parameters.to_numpy()
+        return model_values * slope_01 + offset_01
+
+
+class ObservationFunction01_Bad02(ObservationFunction):
+    # observation parameter variable name does not match corresponding keys
+    def observe(self, model_values):
+        slope_01 = self.observation_parameters['slope_01']
+        some_offset = self.observation_parameters['offset_01']
+        return model_values * slope_01 + some_offset
+
+
 class ObservationFunctionLibrary():
 
     names = [
-        'obsfun01'
+        'obsfun01',
+        'obsfun02',
     ]
 
     observation_functions = {
         'obsfun01' : ObservationFunction01,
+        'obsfun02' : ObservationFunction02,
     }
 
     observation_function_parameters = {
         'obsfun01' : {'slope_01' : 2, 'offset_01' : 10},
+        'obsfun02' : {'p1' : 1.0, 'p2' : 2.0, 'p3' : 3.0, 'p4' : 4.0, 'p5' : 5.0},
     }
 
     observed_states = {
         'obsfun01' : 'y0',
+        'obsfun02' : 'y1',
     }
+
+    variants_obsfun01 = [
+        ObservationFunction01,
+    ]
+
+    variants_obsfun02 = [
+        ObservationFunction02,
+        ObservationFunction02_V02,
+        ObservationFunction02_V03,
+        ObservationFunction02_V04
+    ]
+
+
+    bad_variants_obsfun01 = [
+        ObservationFunction01_Bad01,
+        ObservationFunction01_Bad02,
+    ]
 
